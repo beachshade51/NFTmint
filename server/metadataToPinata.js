60be7bb372e6bed496cb1f826c29ae67908f7cf6
Sidharth P <sidharthp@phaethon.tech>
2023-03-17 12:49:02 +0530

    updated from nodemon

--- server/metadataToPinata.js
+++ server/metadataToPinata.js
@@ -18,48 +18,75 @@ const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY, provider);
 const smartContract = new ethers.Contract(contractAddress, ABI, signer);
 
+const checkDir = (i, pinJson) => {
+   try {
+      // check if directory already exists
+      if (!fs.existsSync(metadataDir)) {
+         fs.mkdirSync(metadataDir);
+         console.log("Directory is created.");
+         fs.writeFileSync(`${metadataDir}/${i}.json`, JSON.stringify(pinJson));
+      } else {
+         fs.writeFileSync(`${metadataDir}/${i}.json`, JSON.stringify(pinJson));
+      }
+   } catch (err) {
+      console.log(err);
+   }
+}
+
+const getAttribute = (key, value) => {
+   return {
+      "trait_type": key,
+      "value": value
+   }
+}
 
 const pinMetaDataToPinata = async (ipfsHash, number, sampleJson) => {
-   const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
-   for (let i = 0; i <= number; i++) {
-
-      let pinJson = {
-         "name": `React.0#${i}`,
-         "image": `https://gateway.pinata.cloud/ipfs/${ipfsHash}/${i}.jpeg`,
-         "description": "Awesome NFT Created by sid21.0",
-         "attributes": []
-      }
-      pinJson["attributes"].push(getAttribute('Icons', sampleJson[i].Icons));
-      pinJson["attributes"].push(getAttribute('Material', sampleJson[i].Material));
-      pinJson["attributes"].push(getAttribute('Plaques', sampleJson[i].Plaques));
-      pinJson["attributes"].push(getAttribute('Frames', sampleJson[i].Frames));
-      pinJson["attributes"].push(getAttribute('Backgrounds', sampleJson[i].Backgrounds));
-      console.log(pinJson)
-      try {
-         // check if directory already exists
-         if (!fs.existsSync(metadataDir)) {
-            fs.mkdirSync(metadataDir);
-            console.log("Directory is created.");
-            fs.writeFileSync(`${metadataDir}/${i}.json`, JSON.stringify(pinJson));
-         } else {
-            fs.writeFileSync(`${metadataDir}/${i}.json`, JSON.stringify(pinJson));
-         }
-      } catch (err) {
-         console.log(err);
-      }
-   }
-   const {
-      dirs,
-      files
-   } = await rfs.read(metadataDir);
-   let data = new FormData();
-   for (const file of files) {
-      data.append(`file`, fs.createReadStream(file), {
-         filepath: basePathConverter(metadataDir, file),
-      });
-   }
-
    try {
+      const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+      for (let i = 0; i < number; i++) {
+
+         let pinJson = {
+            "name": `React.0#${i}`,
+            "image": `https://gateway.pinata.cloud/ipfs/${ipfsHash}/${i}.jpeg`,
+            "description": "Awesome NFT Created by sid21.0",
+            "attributes": []
+         }
+         pinJson["attributes"].push(getAttribute('Icons', sampleJson[i].Icons));
+         pinJson["attributes"].push(getAttribute('Material', sampleJson[i].Material));
+         pinJson["attributes"].push(getAttribute('Plaques', sampleJson[i].Plaques));
+         pinJson["attributes"].push(getAttribute('Frames', sampleJson[i].Frames));
+         pinJson["attributes"].push(getAttribute('Backgrounds', sampleJson[i].Backgrounds));
+
+         console.log(pinJson)
+
+         checkDir(i, pinJson);
+
+         const {
+            dirs,
+            files
+         } = await rfs.read(metadataDir);
+         let data = new FormData();
+         for (const file of files) {
+            data.append(`file`, fs.createReadStream(file), {
+               filepath: basePathConverter(metadataDir, file),
+            });
+         }
+
+         console.log("reached here")
+
+
+         await mintNFT(url, data, files);
+      }
+   }
+   catch (error) {
+      console.log(error)
+   }
+}
+
+async function mintNFT(url, data, files) {
+   try {
+
+      // console.log("reached here hahaha 1", url, data, files)
       let res = await axios.post(url,
          data, {
          headers: {
@@ -70,23 +97,25 @@ const pinMetaDataToPinata = async (ipfsHash, number, sampleJson) => {
       }
       )
 
-      for (let i = 0; i <= files.length; i++) {
+      // console.log(res)
+
+      console.log("reached here 2")
+
+      for (let i = 0; i < files.length; i++) {
          console.log(`https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}/${i}.json`);
-         // let txn = await smartContract.mintNFT(signer.address, `https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}/${i}.json`);
+         let txn = await smartContract.mintNFT(signer.address, `https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}/${i}.json`);
+         console.log(txn.hash);
       }
+
+      console.log("reached here 3")
 
 
    } catch (error) {
+      console.log("reached here 4")
       console.log(error)
    }
 }
 
-const getAttribute = (key, value) => {
-   return {
-      "trait_type": key,
-      "value": value
-   }
-}
 module.exports = {
    pinMetaDataToPinata
 };
